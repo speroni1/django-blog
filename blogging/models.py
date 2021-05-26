608063b99ca5b94fc1a2a3fe366fec85c28f174a
speroni1 <69496401+speroni1@users.noreply.github.com>
2021-05-25 23:32:54 -0400

    Activity 7 Complete
    Activity 7 Complete

--- blogging/models.py
+++ blogging/models.py
@@ -19,3 +19,19 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+
+
+class Category(models.Model):
+    # name
+    name = models.CharField(max_length=128)
+    # description
+    description = models.TextField(blank=True)
+    # posts
+    # post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    posts = models.ManyToManyField(Post, blank=True, related_name='categories')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
