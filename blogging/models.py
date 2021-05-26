f51445a51e089e6ba06536405e5fd244eab5f66e
speroni1 <69496401+speroni1@users.noreply.github.com>
2021-05-26 00:36:34 -0400

    Assignment 7 Complete
    Assignment 7 Complete

--- blogging/models.py
+++ blogging/models.py
@@ -3,6 +3,22 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Category(models.Model):
+    # name
+    name = models.CharField(max_length=128)
+    # description
+    description = models.TextField(blank=True)
+    # posts
+    # post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    # posts = models.ManyToManyField(Post, blank=True, related_name='categories')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
 class Post(models.Model):
     #title
     title = models.CharField(max_length=128)
@@ -16,22 +32,11 @@ class Post(models.Model):
     modified_date = models.TimeField(auto_now=True)
     #published_date
     published_date = models.TimeField(blank=True, null=True)
+    #category
+    categories = models.ManyToManyField(Category, blank=True )
     
     def __str__(self):
         return self.title
 
 
-class Category(models.Model):
-    # name
-    name = models.CharField(max_length=128)
-    # description
-    description = models.TextField(blank=True)
-    # posts
-    # post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    posts = models.ManyToManyField(Post, blank=True, related_name='categories')
 
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name_plural = 'Categories'
