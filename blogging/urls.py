e574190ebd65e55499d18a6e8dad5c8964010e78
speroni1 <69496401+speroni1@users.noreply.github.com>
2021-06-01 23:15:48 -0400

    Assignment 8 Complete
    Assignment 8 Complete

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,9 +1,10 @@
 
+from blogging.views import BlogDetailView, BlogListView
 from django.urls import path, include
-from blogging.views import stub_view, list_view, detail_view
+# from blogging.views import stub_view, list_view, detail_view
 
 urlpatterns = [
     # path('', stub_view, name="blog_index"),
-    path('', list_view, name="blog_index"),
-    path('posts/<int:post_id>/', detail_view, name="blog_detail"),
+    path('', BlogListView.as_view(), name="blog_index"),
+    path('posts/<int:pk>/', BlogDetailView.as_view(), name="blog_detail"),
 ]
